177ac8ae7a32f1e894a5673c07e9caf6f3b5d732
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-03-16 08:15:46 -0600

    GUI login

--- uPhotos/src/app/login/login.component.ts
+++ uPhotos/src/app/login/login.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private username : string = '';
+  private password : string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    alert('Login: ' + this.username + ' - ' + this.password);
+  }
+
+  captura(){
+    alert('Captura');
+  }
+
+  user(event: any) { // without type info
+    this.username = event.target.value;
+  }
+
+  pass(event: any) { // without type info
+    this.password = event.target.value;
+  }
 }
