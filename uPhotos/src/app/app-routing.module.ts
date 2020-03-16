15abc6fffede6e390c3486f6c213aa1247fff719
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-03-16 14:29:32 -0600

    NavBar agregado

--- uPhotos/src/app/app-routing.module.ts
+++ uPhotos/src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { SigninComponent } from './signin/signin.component';
+import { MeComponent } from './me/me.component';
+import { AlbumComponent } from './album/album.component';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'me', component: MeComponent },
+  { path: 'album', component: AlbumComponent },
   { path : '', component : LoginComponent }
 ];
 
--- uPhotos/src/app/app.module.ts
+++ uPhotos/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WebcamModule } from 'ngx-webcam';
+import { SigninComponent } from './signin/signin.component';
+import { AlbumComponent } from './album/album.component';
+import { MeComponent } from './me/me.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserComponent
+    UserComponent,
+    SigninComponent,
+    AlbumComponent,
+    MeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
